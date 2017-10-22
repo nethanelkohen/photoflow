8f245b2b63c45b0c5390b86b77f09935976c3e51
lester <lester@theblumarket.com>
2017-10-22 16:09:36 -0400

    Added some front end

---
+++ photoflow/assets/main.js
@@ -0,0 +1,45 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+function helloWorld(name) {
+    if (name) {
+        console.log(`Hello ${name}, and welcome to the world of computing!`);
+    } else {
+        console.log('Hello world!');
+    }
+}
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $(".image-upload-wrap").hide();
+
+      $(".file-upload-image").attr("src", e.target.result);
+      $(".file-upload-content").show();
+
+      $(".image-title").html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    removeUpload();
+  }
+}
+
+function removeUpload() {
+  $(".file-upload-input").replaceWith($(".file-upload-input").clone());
+  $(".file-upload-content").hide();
+  $(".image-upload-wrap").show();
+}
+$(".image-upload-wrap").bind("dragover", function() {
+  $(".image-upload-wrap").addClass("image-dropping");
+});
+// $(".image-upload-wrap").bind("dragleave", function() {
+//   $(".image-upload-wrap").removeClass("image-dropping");
+// });
