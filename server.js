d3fc552822044977f499a23c9d07f72dfe5f685e
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-10-26 09:25:38 -0400

    tweaked package.json for heroku and added heroku code to server

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ var connection = require('./utility/sql.js');
 var Comments = require('./models/comments.js');
 var app = express();
 
+// might be needed to deploy on heroku
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+
 
 // require authentication middleware
 
