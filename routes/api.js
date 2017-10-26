dba098ec047b283810a7c81e79c381fdb22d3f50
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-10-26 08:39:13 -0400

    deleted api routes

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-// require models
-var comments = require('../models/comments.js');
-var likes = require('../models/likes.js');
-var photos = require('../models/photos.js');
-var users = require('../models/user.js');
-
-module.exports = router;
