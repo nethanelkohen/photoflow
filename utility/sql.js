7fa4b38118d2b88990e40be31f0203346d0659a2
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-10-25 20:26:16 -0400

    fixed sql

--- utility/sql.js
+++ utility/sql.js
@@ -7,15 +7,30 @@ if (process.env.DATABASE_URL) {
 }
 else {
 	sql = new Sequelize({
-		database: "photoflow",
-		username: "lester",
-		password: "",
+		database: process.env.DB_NAME,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
 		host: process.env.DB_HOST || "localhost",
 		port: process.env.DB_PORT || 5432,
 		dialect: "postgres",
 		logging: false,
 	});
 }
+
+// if (process.env.DATABASE_URL) {
+// 	sql = new Sequelize(process.env.DATABASE_URL);
+// }
+// else {
+// 	sql = new Sequelize({
+// 		database: "photoflow",
+// 		username: "lester",
+// 		password: "",
+// 		host: process.env.DB_HOST || "localhost",
+// 		port: process.env.DB_PORT || 5432,
+// 		dialect: "postgres",
+// 		logging: false,
+// 	});
+// }
 // var Sequelize = require('sequelize');
 // var connection = new Sequelize('photoflow','lester','',{
 //   dialect: 'postgres'
