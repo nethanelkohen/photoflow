4ca1439ccb798f2d20c4cc2d55994fab283c76db
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-03-12 11:06:25 -0400

    sql lint

--- utility/sql.js
+++ utility/sql.js
@@ -1,4 +1,4 @@
-var Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 var sql;
 
@@ -6,27 +6,13 @@ var sql;
 DON'T CHANGE THIS - WE NEED IT FOR HEROKU, use sql connection below
 */
 
-// if (process.env.DATABASE_URL) {
-//   sql = new Sequelize(process.env.DATABASE_URL);
-// } else {
-//   sql = new Sequelize({
-//     database: process.env.DB_NAME,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST || "localhost",
-//     port: process.env.DB_PORT || 5432,
-//     dialect: "postgres",
-//     logging: false,
-//   });
-// }
-
 if (process.env.DATABASE_URL) {
   sql = new Sequelize(process.env.DATABASE_URL);
 } else {
   sql = new Sequelize({
-    database: 'photoflow',
-    username: 'nethanelkohen',
-    password: '',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
@@ -34,9 +20,18 @@ if (process.env.DATABASE_URL) {
   });
 }
 
-var Sequelize = require('sequelize');
-var connection = new Sequelize('admin', 'lester', '', {
-  dialect: 'postgres'
-});
+// if (process.env.DATABASE_URL) {
+//   sql = new Sequelize(process.env.DATABASE_URL);
+// } else {
+//   sql = new Sequelize({
+//     database: 'photoflow',
+//     username: 'nethanelkohen',
+//     password: '',
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || 5432,
+//     dialect: 'postgres',
+//     logging: false
+//   });
+// }
 
 module.exports = sql;
